a6ec8329db8dbedb80bc5a74e628fdad1d781c41
mahayasa1 <mahayasa.udit@gmail.com>
2025-11-14 17:57:14 +0800

    change

--- resources/views/partials/pengaduan.blade.php
+++ resources/views/partials/pengaduan.blade.php
@@ -2,42 +2,42 @@
     @csrf
 
     <div>
-        <label for="nama" class="block font-semibold text-gray-700">Nama Pengusul *</label>
+        <label for="nama" class="block font-semibold text-gray-700">Nama <span class="text-red-900"><span class="text-red-900">*</span></span></label>
         <input type="text" id="name" name="name" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="email" class="block font-semibold text-gray-700">Email *</label>
+        <label for="email" class="block font-semibold text-gray-700">Email <span class="text-red-900"><span class="text-red-900">*</span></span></label>
         <input type="email" id="email" name="email" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="telp" class="block font-semibold text-gray-700">Nomor Telepon *</label>
+        <label for="telp" class="block font-semibold text-gray-700">Nomor Telepon <span class="text-red-900">*</span></label>
         <input type="text" id="telp" name="telp" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="judul" class="block font-semibold text-gray-700">Judul Pengaduan *</label>
+        <label for="judul" class="block font-semibold text-gray-700">Judul <span class="text-red-900">*</span></label>
         <input type="text" id="judul" name="judul" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="isi" class="block font-semibold text-gray-700">Isi Pengaduan *</label>
+        <label for="isi" class="block font-semibold text-gray-700">Isi <span class="text-red-900">*</span></label>
         <textarea id="isi" name="isi" rows="4" class="w-full border rounded px-3 py-2" required></textarea>
     </div>
 
     <div>
-        <label for="tanggal_pengaduan" class="block font-semibold text-gray-700">Tanggal Kejadian *</label>
+        <label for="tanggal_pengaduan" class="block font-semibold text-gray-700">Tanggal Kejadian <span class="text-red-900">*</span></label>
         <input type="date" id="tanggal_pengaduan" name="tanggal_pengaduan" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="lokasi" class="block font-semibold text-gray-700">Lokasi Kejadian *</label>
+        <label for="lokasi" class="block font-semibold text-gray-700">Lokasi Kejadian <span class="text-red-900">*</span></label>
         <input type="text" id="lokasi" name="lokasi" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="instansi" class="block font-semibold text-gray-700">Instansi Tujuan *</label>
+        <label for="instansi" class="block font-semibold text-gray-700">Instansi Tujuan <span class="text-red-900">*</span></label>
         <input type="text" id="instansi" name="instansi" class="w-full border rounded px-3 py-2" required>
     </div>
 
--- resources/views/partials/permintaan.blade.php
+++ resources/views/partials/permintaan.blade.php
@@ -2,37 +2,37 @@
     @csrf
 
     <div>
-        <label for="nama" class="block font-semibold text-gray-700">Nama Pengusul *</label>
+        <label for="nama" class="block font-semibold text-gray-700">Nama <span class="text-red-900">*</label>
         <input type="text" id="name" name="name" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="email" class="block font-semibold text-gray-700">Email *</label>
+        <label for="email" class="block font-semibold text-gray-700">Email <span class="text-red-900">*</label>
         <input type="email" id="email" name="email" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="telp" class="block font-semibold text-gray-700">Nomor Telepon *</label>
+        <label for="telp" class="block font-semibold text-gray-700">Nomor Telepon <span class="text-red-900">*</label>
         <input type="text" id="telp" name="telp" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="judul" class="block font-semibold text-gray-700">Judul Permintaan *</label>
+        <label for="judul" class="block font-semibold text-gray-700">Judul <span class="text-red-900">*</label>
         <input type="text" id="judul" name="judul" class="w-full border rounded px-3 py-2" required>
     </div>
 
     <div>
-        <label for="isi" class="block font-semibold text-gray-700">Isi Permintaan *</label>
+        <label for="isi" class="block font-semibold text-gray-700">Isi <span class="text-red-900">*</label>
         <textarea id="isi" name="isi" rows="4" class="w-full border rounded px-3 py-2" required></textarea>
     </div>
 
     <div>
-        <label for="asal" class="block font-semibold text-gray-700">Asal Pemohon</label>
+        <label for="asal" class="block font-semibold text-gray-700">Asal</label>
         <input type="text" id="asal" name="asal" class="w-full border rounded px-3 py-2">
     </div>
 
     <div>
-        <label for="instansi" class="block font-semibold text-gray-700">Instansi Tujuan *</label>
+        <label for="instansi" class="block font-semibold text-gray-700">Instansi Tujuan <span class="text-red-900">*</label>
         <input type="text" id="instansi" name="instansi" class="w-full border rounded px-3 py-2" required>
     </div>
 
